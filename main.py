0656e4eca900ad6b19463f76a10815e724f45ddd
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-06-04 17:29:03 +0900

    CSV 파일 저장 완료

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ driver.implicitly_wait(3)
 url = "https://www.starbucks.co.kr/menu/drink_list.do"
 driver.get(url)
 
+csv_filename = "Starbucks_Menu.csv"
+csv_open = open(csv_filename, "w+", encoding="utf8")
+csv_writer = csv.writer(csv_open)
+csv_writer.writerow(("Menu", "Image"))
+
 html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
 drink_list = soup.find_all("li", {"class": "menuDataSet"})
@@ -16,3 +21,4 @@ drink_list = soup.find_all("li", {"class": "menuDataSet"})
 for list in drink_list:
     get_menu = list.find("dd").text
     get_img = list.find("a", {"class": "goDrinkView"}).find("img")["src"]
+    csv_writer.writerow((get_menu, get_img))
