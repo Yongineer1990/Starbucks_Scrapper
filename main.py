cfa31e1617f69d55c1d2e34a05f4773872838e87
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-06-04 17:19:43 +0900

    음료 정보 가져오기 완료

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import requests
+from selenium.webdriver.common.keys import Keys
 import csv
+
+driver = webdriver.Chrome("/Users/yong_macbook/devtool/chromedriver")
+driver.implicitly_wait(3)
+
+url = "https://www.starbucks.co.kr/menu/drink_list.do"
+driver.get(url)
+
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+drink_list = soup.find_all("li", {"class": "menuDataSet"})
+
+for list in drink_list:
+    get_menu = list.find("dd").text
+    get_img = list.find("a", {"class": "goDrinkView"}).find("img")["src"]
